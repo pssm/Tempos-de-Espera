3f1012d0b37dbdb39262d19d28aa906dc67be989
pssm <pedromiguelmendes@outlook.com>
2016-02-13 22:23:26 +0000

    Added the Copyright notice

--- app/src/main/java/pedromendes/tempodeespera/utils/SortedList.java
+++ app/src/main/java/pedromendes/tempodeespera/utils/SortedList.java
@@ -1,78 +1,59 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pedromendes.tempodeespera.utils;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-/**
- * This class is a List implementation which sorts the elements using the
- * comparator specified when constructing a new instance.
- * 
- * @param <T>
- */
+
 public class SortedList<T> extends LinkedList<T> {
-    /**
-     * Needed for serialization.
-     */
+
     private static final long serialVersionUID = 1L;
-    /**
-     * Comparator used to sort the list.
-     */
+
     private Comparator<? super T> comparator = null;
-    /**
-     * Construct a new instance with the list elements sorted in their
-     * {@link java.lang.Comparable} natural ordering.
-     */
+
     public SortedList() {
     }
-    /**
-     * Construct a new instance using the given comparator.
-     * 
-     * @param comparator
-     */
+
     public SortedList(Comparator<? super T> comparator) {
         this.comparator = comparator;
     }
-    /**
-     * Add a new entry to the list. The insertion point is calculated using the
-     * comparator.
-     * 
-     * @param paramT
-     */
+
     @Override
     public boolean add(T paramT) {
         int insertionPoint = Collections.binarySearch(this, paramT, comparator);
         super.add((insertionPoint > -1) ? insertionPoint : (-insertionPoint) - 1, paramT);
         return true;
     }
-    /**
-     * Adds all elements in the specified collection to the list. Each element
-     * will be inserted at the correct position to keep the list sorted.
-     * 
-     * @param paramCollection
-     */
+
     @Override
     public boolean addAll(Collection<? extends T> paramCollection) {
         boolean result = false;
         if (paramCollection.size() > 4) {
             result = super.addAll(paramCollection);
             Collections.sort(this, comparator);
-        }
-        else {
-            for (T paramT:paramCollection) {
+        } else {
+            for (T paramT : paramCollection) {
                 result |= add(paramT);
             }
         }
         return result;
     }
-    /**
-     * Check, if this list contains the given Element. This is faster than the
-     * {@link #contains(Object)} method, since it is based on binary search.
-     * 
-     * @param paramT
-     * @return <code>true</code>, if the element is contained in this list;
-     * <code>false</code>, otherwise.
-     */
+
     public boolean containsElement(T paramT) {
         return (Collections.binarySearch(this, paramT, comparator) > -1);
     }
